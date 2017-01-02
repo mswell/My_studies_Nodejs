1136ccd04d012e89fe49fd70f599fcd977e63077
Wellington Moraes <wellpunk@gmail.com>
2017-01-02 14:01:45 -0200

    add more lines

--- jornadadodev/aula1.js
+++ jornadadodev/aula1.js
@@ -3,10 +3,13 @@ var http = require('http');
 var server = http.createServer(function(request, reponse){
     reponse.writeHead(200, {"Content-Type": "text/html"});
     reponse.write("<h1>Hello world!</h1>");
+    reponse.write("<br>");
+    reponse.write("<br>");
+    reponse.write("<h1>Primeira de nodejs</h1>");
     reponse.end();
 });
 
 server.listen(3000, function(){
-    console.log('Servidor esta rodando!')
+    console.log('Servidor está rodando!')
 });
 
